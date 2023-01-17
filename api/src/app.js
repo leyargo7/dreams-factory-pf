bfea9a0093264b682ee25df299bd464e00de7ffa
Zoranspiegel <zoranbow@gmail.com>
2023-01-17 08:37:04 -0500

    src/data/products.js created!

--- api/src/app.js
+++ api/src/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const morgan = require('morgan')
 const app = express();
-const routes = require("./routes");
 
 //settings
 app.set('port', process.env.PORT || 3001);
@@ -12,6 +11,5 @@ app.use(express.urlencoded({extended: false}));
 
 
 //routes
-app.use(routes)
 
 module.exports = app;
