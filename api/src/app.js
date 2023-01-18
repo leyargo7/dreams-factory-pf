bcca8d5553805047af998391510c7f4df120f37a
john11256 <john11256@hotmail.com>
2023-01-18 16:18:19 -0500

    cors

--- api/src/app.js
+++ api/src/app.js
@@ -16,6 +16,19 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, DELETE"
+    );
+    next();
+});
 
 //routes
 app.use('/api', productRoutes)
