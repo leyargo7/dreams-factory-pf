cc29c220cafa1d30a3b4fd1a905651aa7e775ae7
Juan José Rodriguez <zoranbow@gmail.com>
2023-01-18 20:59:45 -0500

    Navbar, Searchbar created

---
+++ client/src/components/Cards/Cards.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Card from "../Card/Card";
+import s from "./Cards.module.css";
+
+const Cards = ({ currentProducts }) => {
+  return (
+    <div className={s.deck}>
+      {currentProducts?.map((ev) => {
+        return (
+          <div>
+            <Card
+              key={ev._id}
+              title={ev.title}
+              img={ev.img}
+              rating={ev.rating}
+              price={`$${ev.price}`}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Cards;
