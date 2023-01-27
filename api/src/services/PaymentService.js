ef91124f3a33c019d40efee3a12d82e37971a087
Juan José Rodriguez <zoranbow@gmail.com>
2023-01-27 00:41:02 -0500

    mercadopago v-beta

---
+++ api/src/services/PaymentService.js
@@ -0,0 +1,38 @@
+const axios = require("axios");
+
+class PaymentService {
+  async createPayment() {
+    const url = "https://api.mercadopago.com/checkout/preferences";
+
+    const body = {
+      "payer_email":"test_user_1295402328@testuser.com",
+      "items": [
+        {
+          "title": "Dummy Title",
+          "description": "Dummy description",
+          "picture_url": "http://www.myapp.com/myimage.jpg",
+          "category_id": "cat123",
+          "quantity": 1,
+          "unit_price": 10
+        }
+      ],
+      "back_urls": {
+        "success": "https://www.success.com",
+        "failure": "http://www.failure.com",
+        "pending": "http://www.pending.com"
+      },
+      "notification_url": "https://www.your-site.com/ipn"
+      //"notification_url": "https://www.api."
+    };
+
+    const payment = await axios.post(url, body, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.ACCESS_TOKEN}`
+      }
+    });
+    return payment.data;
+  }
+}
+
+module.exports = PaymentService;
