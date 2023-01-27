94296f40d28144f38b6e6b533bcfd41290b7b8dc
Juan José Rodriguez <zoranbow@gmail.com>
2023-01-27 01:26:17 -0500

    payment v-beta

--- api/src/services/PaymentService.js
+++ api/src/services/PaymentService.js
@@ -1,29 +1,31 @@
 const axios = require("axios");
 
 class PaymentService {
-  async createPayment() {
+  async createPayment(bodyFront) {
     const url = "https://api.mercadopago.com/checkout/preferences";
 
-    const body = {
-      "payer_email":"test_user_1295402328@testuser.com",
-      "items": [
-        {
-          "title": "Dummy Title",
-          "description": "Dummy description",
-          "picture_url": "http://www.myapp.com/myimage.jpg",
-          "category_id": "cat123",
-          "quantity": 1,
-          "unit_price": 10
-        }
-      ],
-      "back_urls": {
-        "success": "https://www.success.com",
-        "failure": "http://www.failure.com",
-        "pending": "http://www.pending.com"
-      },
-      "notification_url": "https://www.your-site.com/ipn"
-      //"notification_url": "https://www.api."
-    };
+    const body = JSON.stringify(bodyFront);
+    
+    // const body = {
+    //   "payer_email":"test_user_1295402328@testuser.com",
+    //   "items": [
+    //     {
+    //       "title": "Dummy Title",
+    //       "description": "Dummy description",
+    //       "picture_url": "http://www.myapp.com/myimage.jpg",
+    //       "category_id": "cat123",
+    //       "quantity": 1,
+    //       "unit_price": 10
+    //     }
+    //   ],
+    //   "back_urls": {
+    //     "success": "https://www.success.com",
+    //     "failure": "http://www.failure.com",
+    //     "pending": "http://www.pending.com"
+    //   },
+    //   "notification_url": "https://www.your-site.com/ipn"
+    //   //"notification_url": "https://www.api."
+    // };
 
     const payment = await axios.post(url, body, {
       headers: {
