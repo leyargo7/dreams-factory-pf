8659e4d749d33d81e5fe970b6d85e34684b823d0
unknown <you@example.com>
2023-02-08 09:15:22 -0300

    my orders

--- client/src/components/MyOrders/MyOrders.jsx
+++ client/src/components/MyOrders/MyOrders.jsx
@@ -3,6 +3,7 @@ import { useHistory } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import { getUser } from "../../redux/actions/actions";
 import { useDispatch, useSelector } from "react-redux";
+import OrderCards from "../OrderCards/OrderCards"
 import s from "./MyOrders.module.css";
 import axios from "axios";
 
@@ -33,13 +34,22 @@ const MyOrders = () => {
     }
   }, [history, dispatch, userGoogle._id]);
 
+  useEffect(()=>{
+    if(userGoogle){
+      console.log(userGoogle.purchases);
+    }
+  },[userGoogle])
+
   return (
     <div className={s.orders}>
       <h1>My Orders</h1>
-      {userId && userId.name && <p>Hi {userId.name}!</p>}
+      {userId && userId.name && <h2>Hi {userId.name}!</h2>}
       {userId && userId.email && <p>Email: {userId.email}</p>}
-      {userGoogle && userGoogle.name && <p>Hi {userGoogle.name}!</p>}
+      {userGoogle && userGoogle.name && <h2>Hi {userGoogle.name}!</h2>}
       {userGoogle && userGoogle.email && <p>Email: {userGoogle.email}</p>}
+      <h3>Your last purchase...</h3>
+      {userId.purchases && <OrderCards purchases={userId.purchases}/>}
+      {userGoogle.purchases && <OrderCards purchases={userGoogle.purchases}/>}
     </div>
   );
 };
---
+++ client/src/components/OrderCards/OrderCards.jsx
@@ -0,0 +1,23 @@
+import OrderCard from "../OrderCard/OrderCard";
+import s from "./OrderCards.module.css";
+
+const OrderCards = ({ purchases }) => {
+  return (
+    <div className={s.deck}>
+      {purchases[purchases.length-1].map((p, i) => (
+        <OrderCard
+          key={i}
+          title={p.title}
+          unit_price={p.unit_price}
+          picture_url={p.picture_url}
+          date={p.date}
+          quantity={p.quantity}
+          hour={p.hour}
+        />
+      ))}
+      <h1 className={s.total}>Total Price: ${Math.floor(purchases[purchases.length-1].reduce((acc, e) => acc + (e.unit_price * e.quantity) ,0))}</h1>
+    </div>
+  );
+};
+
+export default OrderCards;
