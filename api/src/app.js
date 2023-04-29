258f450c0b07f2d41849d6241a2414312c8bfb8a
Arley Gonzalez <65101299+leyargo7@users.noreply.github.com>
2023-04-29 14:47:50 -0500

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -32,7 +32,8 @@ app.use(morgan('dev'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors({ origin: `${process.env.FRONT_URL}`, credentials: true }))
+app.use(cors());
+//app.use(cors({ origin: `${process.env.FRONT_URL}`, credentials: true }))
 
 // app.use((req, res, next) => {
 //   res.header("Access-Control-Allow-Origin", "*");
