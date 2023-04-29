a51334a31b27e80c43983c119a1066d03c13fd5a
Arley Gonzalez <65101299+leyargo7@users.noreply.github.com>
2023-04-29 14:29:52 -0500

    update action server
    se actualiza url de server

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,5 @@
-export const SERVER_URL = "https://dreams-factory-pf-production.up.railway.app";
+//export const SERVER_URL = "https://dreams-factory-pf-production.up.railway.app";
 //export const SERVER_URL = "https://c919-186-81-100-12.ngrok.io";
+export const SERVER_URL = "https://dreams-xmlc.onrender.com"
 export const FRONT_URL = "https://dreams-factory-pf.vercel.app";
 // export const SERVER_URL = "http://localhost:3001";
